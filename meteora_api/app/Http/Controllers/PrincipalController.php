09658b0cc8e25059044a376547545028c7ed0afc
Seu Nome <seu_email@example.com>
2024-11-06 21:10:40 -0300

    aula17

--- meteora_api/app/Http/Controllers/PrincipalController.php
+++ meteora_api/app/Http/Controllers/PrincipalController.php
@@ -6,8 +6,15 @@ use Illuminate\Http\Request;
 
 class PrincipalController extends Controller
 {
-    public function principal (){
-        return view('welcome');
-       
+    public function Principal (){
+        $fornecedores = [
+            'Fornecedor 1',
+            'Fornecedor 2',
+            'Fornecedor 3',
+        ];
+            
+        
+        return view ('index', compact('fornecedores'));
     }
 }
+?>
---
+++ meteora_api/resources/views/index.blade.php
@@ -0,0 +1,10 @@
+
+
+<h1>Fornecedor</h1>
+
+@foreach ($fornecedores as $fornecedor)
+<p> Nome: {{$fornecedor}}</p>
+    
+@endforeach
+
+
